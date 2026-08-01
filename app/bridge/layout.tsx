fd0ad52286b9653508c2392e25fe266514ac16c5
traviscomber <n3uralia@gmail.com>
2026-07-31 22:31:45 -0400

    Replace legacy bridge shell with Titan full-screen console

--- app/bridge/layout.tsx
+++ app/bridge/layout.tsx
@@ -1,10 +1,3 @@
-import { Sidebar } from "@/components/layout/Sidebar";
-
 export default function BridgeLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-full min-h-screen">
-      <Sidebar />
-      <div className="flex flex-col flex-1 min-w-0">{children}</div>
-    </div>
-  );
+  return <div className="min-h-screen bg-[#080a0b] text-white">{children}</div>;
 }
