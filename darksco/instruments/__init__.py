ead5daf8282b46ad6142f19577efbb54ce3340ec
traviscomber <n3uralia@gmail.com>
2026-09-12 09:46:49 -0300

    feat(field): export preset controls

--- darksco/instruments/__init__.py
+++ darksco/instruments/__init__.py
@@ -1,10 +1,22 @@
 """Semantic control layers for DARKSCO instruments."""
 
 from .field import FIELD_CONTROLS, FIELD_DEVICE_NAME, FieldControlError, compile_field_state
+from .field_presets import (
+    FIELD_PRESETS,
+    FieldPreset,
+    compile_field_preset,
+    get_field_preset,
+    randomize_field_controls,
+)
 
 __all__ = [
     "FIELD_CONTROLS",
     "FIELD_DEVICE_NAME",
+    "FIELD_PRESETS",
     "FieldControlError",
+    "FieldPreset",
     "compile_field_state",
+    "compile_field_preset",
+    "get_field_preset",
+    "randomize_field_controls",
 ]
