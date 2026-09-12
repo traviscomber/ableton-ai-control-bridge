097d57c11a5750a9afcff3a92673332e1bef6618
traviscomber <n3uralia@gmail.com>
2026-09-12 09:51:14 -0300

    feat(field): export intent API

--- darksco/instruments/__init__.py
+++ darksco/instruments/__init__.py
@@ -1,6 +1,13 @@
 """Semantic control layers for DARKSCO instruments."""
 
 from .field import FIELD_CONTROLS, FIELD_DEVICE_NAME, FieldControlError, compile_field_state
+from .field_intent import (
+    FIELD_INTENT_DELTAS,
+    FieldIntent,
+    compile_field_intent,
+    parse_field_phrase,
+    resolve_field_intent,
+)
 from .field_presets import (
     FIELD_PRESETS,
     FieldPreset,
@@ -12,11 +19,16 @@ from .field_presets import (
 __all__ = [
     "FIELD_CONTROLS",
     "FIELD_DEVICE_NAME",
+    "FIELD_INTENT_DELTAS",
     "FIELD_PRESETS",
     "FieldControlError",
+    "FieldIntent",
     "FieldPreset",
+    "compile_field_intent",
     "compile_field_state",
     "compile_field_preset",
     "get_field_preset",
+    "parse_field_phrase",
     "randomize_field_controls",
+    "resolve_field_intent",
 ]
